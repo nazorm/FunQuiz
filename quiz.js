dbefa00901eb3e1e86116443ff0af1aa90d6c22a
Nazorm <nnadoziechinazor@gmail.com>
2020-06-22 00:16:56 +0100

    Changes made to main js

--- quiz.js
+++ quiz.js
@@ -2,25 +2,24 @@ var a = document.querySelector(".interestSection");
 var b = document.querySelector(".avatar");
 var quizSection = document.querySelector(".quizSection");
 
-function EntryBtn(){  
- var userName = document.querySelector(".UserAvater");
- userName.innerHTML = "Hello," + " " + b.value + " " +"😊";
-    if (a.style.display === "block"){
-        a.style.display = "none";
-    }else{
-        a.style.display = "block";
-    }
+function EntryBtn() {
+  var userName = document.querySelector(".UserAvater");
+  userName.innerHTML = "Hello," + " " + b.value + " " + "😊";
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
 }
 
-function startQuiz(){
-    if(quizSection.style.display === "block"){
-        quizSection.style.display = "none";
-    }else{
-        quizSection.style.display = "block";
-    } 
-      questionOne();
+function startQuiz() {
+  if (quizSection.style.display === "block") {
+    quizSection.style.display = "none";
+  } else {
+    quizSection.style.display = "block";
+  }
+  updateQuestion();
 }
-/*Getting hold of elements in Quiz Section */
 
 var questionNumber = document.querySelector(".questionNumber");
 var question = document.querySelector(".question");
@@ -29,95 +28,124 @@ var option2 = document.querySelector(".option2");
 var option3 = document.querySelector(".option3");
 var option4 = document.querySelector(".option4");
 var pointScore = document.querySelector(".pointScore");
-
-
 var options = document.querySelector(".options");
 
+var questions = [
+  {
+    question: "how many classes of food are there?",
+    options: ["9", "5", "7", "8"],
+    answer: 2
+  },
 
-function questionOne(){
-    
-    questionNumber.innerHTML = " " + 1;
-    
-   question.innerHTML = "Who is the APC Chairman that has just been suspended ?"
-  
-   option1.innerHTML = "Babatunde Fashola";
+  {
+    question: "where is the center of the earth located?",
+    options: ["Ethiopia", "Tanzania", "America", "Turkey"],
+    answer: 3
+  },
 
-   option2.innerHTML = "Izeiyamu";
-  
-   option3.innerHTML = "Oshiomole";
-   
-   option4.innerHTML = "Answer it yourself";
+  {
+    question: "who was the first woman on Earth?",
+    options: ["Medusa", "Athena", "Pandora", "Rhea"],
+    answer: 2
+  },
+  {
+    question: "where did the first man originate from?",
+    options: ["Cameroon", "Ethiopia", "Egypt", "Isreal"],
+    answer: 1
+  },
+  {
+    question: "what is the loudest animal on Earth?",
+    options: ["Elephant", "Sperm Whale", "Rhinoseros", "Hyena"],
+    answer: 1
+  },
+  {
+    question: "How many ribs are in the human body?",
+    options: ["9", "10", "14", "24"],
+    answer: 3
+  },
+  {
+    question: "what is the most common colour of toilet paper in France?",
+    options: ["Yellow", "White", "Pink", "Grey"],
+    answer: 2
+  },
+  {
+    question: "The offspring of a Donkey and Zibra is called what?",
+    options: ["kid", "Zeedonk", "Zibling", "Zeebudonk"],
+    answer: 1
+  },
+  {
+    question: "what year did the French Revolution end?",
+    options: ["4th of July", "1900", "1700", "1799"],
+    answer: 3
+  },
+  {
+    question: "What is the full meaning of MAC in Apple?",
+    options: ["Macaroni", "Macaroni", "Macintosh", "Steve Jobs"],
+    answer: 2
+  }
+];
 
-   
+let currentQuestion = 0;
+let point = 0;
+pointScore.innerHTML = point;
+
+
+function updateQuestion() {
+  //Back to default color
+  option1.style.backgroundColor = "#0080ff";
+  option2.style.backgroundColor = "#0080ff";
+  option3.style.backgroundColor = "#0080ff";
+  option4.style.backgroundColor = "#0080ff";
+
+  questionNumber.innerHTML = currentQuestion + 1;
+
+  question.innerHTML = questions[currentQuestion].question;
+  option1.innerHTML = questions[currentQuestion].options[0];
+  option2.innerHTML = questions[currentQuestion].options[1];
+  option3.innerHTML = questions[currentQuestion].options[2];
+  option4.innerHTML = questions[currentQuestion].options[3];
 }
 
+function handleNextbtn() {
+  currentQuestion += 1;
+
+  if (currentQuestion === 10) {
+    alert(`your score is ${point}`);
+    
+  }
+
+  updateQuestion();
+}
+
+function handlePrevbtn() {
+  currentQuestion -= 1;
+  if (currentQuestion === 0) {
+    alert("first question");
+    
+  }
+  updateQuestion();
+}
 
 const option = (index) => {
-    if (index === 2){
-        pointScore.innerHTML = Number(pointScore.innerHTML) + 1; 
-        option1.style.backgroundColor = "red";
-        option2.style.backgroundColor = "red";
-        option3.style.backgroundColor = "green";
-        option4.style.backgroundColor = "red";
-    }else{
-        option1.style.backgroundColor = "red";
-        option2.style.backgroundColor = "red";
-        option3.style.backgroundColor = "green";
-        option4.style.backgroundColor = "red";
-    }
-
-    }
-    function submitbtn(){
-
-
-
-
-         /*Back to default color*/
-         option1.style.backgroundColor = "#0080ff";
-         option2.style.backgroundColor = "#0080ff";
-         option3.style.backgroundColor = "#0080ff";
-         option4.style.backgroundColor = "#0080ff";
-
-
-
-
-
-   questionNumber.innerHTML = " " + 2;
-    
-   question.innerHTML = "Who is Tekashi SixNine?"
-  
-   option1.innerHTML = "Boo Harry";
-
-   option2.innerHTML = "American Musician";
-  
-   option3.innerHTML = "Nicki Minaj's Hubby";
-   
-   option4.innerHTML = "I don't Snitch";
-  
-    /* Right colors for question 2 */
-        options.addEventListener("click", function(){ 
-        option1.style.backgroundColor = "red";
-        option2.style.backgroundColor = "green";
-        option3.style.backgroundColor = "red";
-        option4.style.backgroundColor = "red";
-        });
-   
-        option2.addEventListener("click", function(){
-        pointScore.innerHTML = Number(pointScore.innerHTML) + 1; 
-      })
-
-
-     
-       option3.addEventListener("click", function(){
-        pointScore.innerHTML = Number(pointScore.innerHTML) - 1; 
-    })
-
-
-
-
-
-
-
-
+  let answer = questions[currentQuestion].answer;
+  if (index == answer) {
+    point += 1;
+    pointScore.innerHTML = point;
+  }
+  option1.style.backgroundColor = "red";
+  option2.style.backgroundColor = "red";
+  option3.style.backgroundColor = "red";
+  option4.style.backgroundColor = "red";
+  if (answer === 0) {
+    option1.style.backgroundColor = "green";
+  }
+  if (answer === 1) {
+    option2.style.backgroundColor = "green";
+  }
+  if (answer === 2) {
+    option3.style.backgroundColor = "green";
+  }
+  if (answer === 3) {
+    option4.style.backgroundColor = "green";
+  }
 };
-   
